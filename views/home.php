438e7697e592bfb66f6db1648e51efd67a9c5c01
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-12-31 09:02:24 -0200

    Estrutura MVC completa mais jquery e template e css, js INICIAIS e bootstrap

--- views/home.php
+++ views/home.php
@@ -1,11 +1,14 @@
 <form method="POST">
 Escolha o módulo:<br/>
-<select name="modulos">
-
+<select name="modulos" id="modulos">
+	<option></option>
+	<?php foreach ($modulos as $modulo): ?>
+	<option value="<?php echo $modulo['id']?>"><?php echo $modulo['titulo']?></option>
+	<?php endforeach; ?>
 </select><br/><br/>
 
 Escolha a aula:<br/>
-<select name="aulas">
+<select name="aulas" id="aulas">
 
 </select><br/><br/>
 
