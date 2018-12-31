4c2c4ad0e584cf2b0bf13a888599d8cc12e8c8bc
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-12-31 08:37:58 -0200

    Armazenando os dados de modulos no controller Home

--- models/Modulos.php
+++ models/Modulos.php
@@ -2,4 +2,18 @@
 
 class Modulos extends  model{
     
+    public function getList(){
+        
+        $array = array();
+        
+        $sql = "SELECT * FROM modulos";
+        $sql = $this->db->query($sql);
+        
+        if($sql->rowCount() > 0){
+            $array = $sql->fetchAll();
+        }
+        
+        return $array;
+    }
+    
 }
