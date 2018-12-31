13b2c6eed4d01e7f92caac53e58cf960ebc09872
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-12-31 09:04:55 -0200

    Estrutura MVC completa mais jquery e template e css, js e bootstrap INICIAIS

--- views/template.php
+++ views/template.php
@@ -3,6 +3,7 @@
 		<meta charset="utf-8">
 		<title>MVC</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL; ?>assets/css/style.css" />
 		
 	</head>
@@ -10,6 +11,7 @@
 	<body>
 		<?php $this->loadViewInTemplate($viewName, $viewData); ?>
 		<script type="text/javascript">var BASE_URL = '<?php echo BASE_URL; ?>';</script>
+		<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/bootstrap.min.js" ></script>
 		<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/jquery-3.3.1.min.js.js" ></script>
 		<script type="text/javascript" src="<?php echo BASE_URL; ?>assets/js/javascript.js" ></script>	
 	</body>
